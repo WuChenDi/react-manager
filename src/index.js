3a55f5eafcc171aca277267498a5cec0caf0c568
WuChenDi <996194720@qq.com>
2018-08-09 23:00:38 +0800

    header footer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Life from './pages/demo/Life';
+import Admin from './admin';
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Life />, document.getElementById('root'));
+// ReactDOM.render(<Life />, document.getElementById('root'));
+ReactDOM.render(<Admin />, document.getElementById('root'));
 registerServiceWorker();
