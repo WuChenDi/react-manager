9a74df5d9329586dabda5315be8b07f31cce99c6
WuChenDi <996194720@qq.com>
2018-08-09 23:10:17 +0800

    code

--- src/admin.js
+++ src/admin.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
 
 export default class Admin extends React.Component {
   render() {
     return (
       <Row>
         <Col span="3">
-          Left
+          <NavLeft />
         </Col>
         <Col span="21">
           <Header>
