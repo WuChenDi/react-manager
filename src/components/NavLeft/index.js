5eecd9c3875e3f42a93b5e4378351cb7a195c578
WuChenDi <996194720@qq.com>
2018-08-20 20:25:23 +0800

    820update

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class NavLeft extends React.Component{
+  render(){
+    var style = {
+      backgroundColor: 'red'
+    }
+    return(
+      <div style = {style}>
+        This is NavLeft
+      </div>
+    )
+  }
+}
