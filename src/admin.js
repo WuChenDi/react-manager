b33a224fdb9e2995161df8f14cadd8f84124012e
WuChenDi <996194720@qq.com>
2018-08-20 22:37:59 +0800

    add code

--- src/admin.js
+++ src/admin.js
@@ -3,15 +3,15 @@ import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
-
+import './style/common.less'
 export default class Admin extends React.Component {
   render() {
     return (
       <Row className="container">
-        <Col span="3" className="nav-left">
+        <Col span="4" className="nav-left">
           <NavLeft />
         </Col>
-        <Col span="21" className="main">
+        <Col span="20" className="main">
           <Header />
           <Row className="content">
             content
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Menu,Icon} from 'antd';
+import MenuConfig from '../../config/menuConfig'
+import './index.less'
+const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component{
   render(){
     return(
-      <div style = {{backgroundColor: 'red'}}>
-        This is NavLeft
+      <div>
+        <div>
+          <div className="logo">
+            <img src="/assets/logo-ant.svg" alt=""/>
+            <h1>Imooc MS</h1>
+          </div>
+        </div>
+        <Menu theme="dark">
+          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+            <Menu.Item key="3">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </SubMenu>
+        </Menu>
       </div>
     )
   }
