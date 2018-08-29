ea823239a3b20f45aa98b8d2ae586c3a9fdaac9b
WuChenDi <996194720@qq.com>
2018-08-29 23:34:37 +0800

    调用天气接口

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,16 @@
 import JsonP from 'jsonp'
 export default class Axios {
-  static jsonp() {
+  static jsonp(options) {
     return new Promise((resolve, reject) => {
       JsonP(options.url, {
         param: 'callback'
       }, function (err, response) {
         // to-do
+        if (response.status == 'success') {
+          resolve(response);
+        } else {
+          reject(response.message);
+        }
       })
     })
   }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,8 +24,14 @@ export default class Header extends React.Component {
     let city = '广州'
     axios.jsonp({
       url: 'http://api.map.baidu.com/telematics/v3/weather?location=' + encodeURIComponent(city) + '&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
-    }).then(() => {
-      
+    }).then((res) => {
+      if (res.status == 'success') {
+        let data = res.results[0].weather_data[0];
+        this.setState({
+          dayPictureUrl: data.dayPictureUrl,
+          weather: data.weather
+        })
+      }
     })
   }
   render() {
@@ -43,7 +49,12 @@ export default class Header extends React.Component {
           </Col>
           <Col span="20" className="weather">
             <span className="date">{this.state.sysTime}</span>
-            <span className="weather-detail">晴转多云</span>
+            <span className="weather-img">
+              <img src={this.state.dayPictureUrl} alt="" />
+            </span>
+            <span className="weather-detail">
+              {this.state.weather}
+            </span>
           </Col>
         </Row>
       </div>
