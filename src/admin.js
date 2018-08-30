06b7c5d47c899d362745610a48002f7ec285a4e4
WuChenDi <996194720@qq.com>
2018-08-30 21:33:09 +0800

    home

--- src/admin.js
+++ src/admin.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
 import './style/common.less'
+import Home from './pages/home'
 export default class Admin extends React.Component {
   render() {
     return (
@@ -14,7 +15,7 @@ export default class Admin extends React.Component {
         <Col span="20" className="main">
           <Header />
           <Row className="content">
-            content
+            <Home />
             {/* {this.props.children} */}
           </Row>
           <Footer />
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.less'
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-warp">
+        study IMooc the Backstage Management System
+      </div>
+    )
+  }
+}
