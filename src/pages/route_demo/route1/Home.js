4e065358698c8749b3b62f8177d5dd5e2bc18031
WuChenDi <996194720@qq.com>
2018-08-31 21:31:55 +0800

    zujian

--- src/pages/route_demo/route1/Home.js
+++ src/pages/route_demo/route1/Home.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import {HashRouter,Route,Link} from 'react-router-dom'
+import { HashRouter, Route, Link } from 'react-router-dom'
 
 export default class Home extends React.Component {
   render() {
     return (
       <HashRouter>
         <div>
-        <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+          <hr/>
+          <Route path="/" component={Main}></Route>
         </div>
         <Route path="/about"></Route>
         <Route path="/topic"></Route>
