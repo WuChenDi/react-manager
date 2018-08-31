43948de0b292b3660366456b18441dffca4494d9
WuChenDi <996194720@qq.com>
2018-08-31 21:25:36 +0800

    route_demo

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 // import App from './App';
 import Life from './pages/demo/Life';
 import Admin from './admin';
+import Home from './pages/route_demo/route1/Home';
+
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<Life />, document.getElementById('root'));
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
