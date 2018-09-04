bdee17e22a1df2104c9df12d3640fbee30281f68
WuChenDi <996194720@qq.com>
2018-09-04 22:08:43 +0800

    login

--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
 import { HashRouter, Route, Switch } from 'react-router-dom'
+import App from './App'
+import Login from './pages/login'
+import Admin from './admin';
 export default class IRouter extends React.Component {
   render() {
     return (
       <HashRouter>
-        
+        <App>
+          <Route path="/login" component={Login} />
+          <Route path="/admin" component={Admin} />
+        </App>
       </HashRouter>
     );
   }
