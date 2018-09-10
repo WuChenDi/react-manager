8d7eee549b833feab13851fb10b083936d8d5351
WuChenDi <996194720@qq.com>
2018-09-10 08:34:47 +0800

    9.10 update modals.js

--- src/pages/ui/modals.js
+++ src/pages/ui/modals.js
@@ -16,10 +16,10 @@ export default class Buttons extends React.Component {
     return (
       <div>
         <Card title="基础模态框">
-          <Button type="primary" onClick={this.handleOpen('showModal1')}>Open</Button>
-          <Button type="primary" onClick={this.handleOpen('showModal2')}>自定义页脚</Button>
-          <Button type="primary" onClick={this.handleOpen('showModal3')}>顶部20px弹框</Button>
-          <Button type="primary" onClick={this.handleOpen('showModal4')}>水平垂直居中</Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal1')}>Open</Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal2')}>自定义页脚</Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal3')}>顶部20px弹框</Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal4')}>水平垂直居中</Button>
         </Card>
         <Modal title="React" visible={this.state.showModal1} onCancel={() => { }}>
           <p>WuChenDi系统</p>
