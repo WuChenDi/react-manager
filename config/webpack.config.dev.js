9d7e37b7df844efc4cf1aaeb5069ac86ed996a03
WuChenDi <996194720@qq.com>
2018-09-13 09:23:56 +0800

    更改主题颜色

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -196,7 +196,7 @@ module.exports = {
                 options: {
                   modules:false,
                   modifyVars: {
-                    "@primary-color": "#f9c700"
+                    "@primary-color": "#1890ff"
                   }
                 }
               }
